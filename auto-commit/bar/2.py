7aac0e56c752c802e3acbd1ef9f9f5271c4d0d49
yue.zhang <zhangyue9306@gmail.com>
2024-01-05 18:51:46 +0800

    t

--- auto-commit/bar/2.py
+++ auto-commit/bar/2.py
@@ -1,43 +1,36 @@
-def solution(s):
-    def _is_str(ch):
-        return ord("a") <= ord(ch) <= ord("z")
-
-    def _is_num(ch):
-        return ch in {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+def solution(s: str) -> str:
+    def _split(st):
+        if st == "":
+            return "", 1
+        i = 0
+        r = ""
+        while i < len(st):
+            if st[i].isdigit():
+                break
+            r += st[i]
+            i += 1
+        if i < len(st):
+            return r, int(st[i:])
+        return r, 1
 
     ans = ""
     ms = []
-    tmp2 = ""
-    i = 0
-    while i < len(s):
-        print(i)
-        if s[i] == "]":
-            while i < len(s) and s[i] == "]" and len(ms):
-                chs = ms.pop()
-                k = 1
-                if len(ms) and _is_num(ms[-1]):
-                    k = int(ms.pop())
-                tmp2 = (chs+tmp2) * k
-                print("pop", chs, i, tmp2, ms)
-                i += 1
-            ans += tmp2
-            tmp2 = ""
-            continue
-        while i < len(s) and s[i] != "[":
-            tmp = ""
-            while i < len(s) and _is_str(s[i]):
-                tmp += s[i]
-                print("here")
-                i += 1
-            if len(tmp):
-                ms.append(tmp)
-            if _is_num(s[i]):
-                ms.append(s[i])
-                i += 1
-            break
-        if s[i] == "[":
-            i += 1
-        print(ms)
+    for ch in s:
+        if ch == "[":
+            ms.append(ans)
+            ans = ""
+        elif ch == "]":
+            ms.append(ans)
+            ans = ""
+            fst = ms.pop()
+            if ms:
+                snd = ms.pop()
+            else:
+                snd = ""
+            st, num = _split(snd)
+            ans = st + num * fst
+        else:
+            ans += ch
     return ans
 
 
